551ad37f30bbd77ce1fc7af989c43e2876e62463
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-09-28 03:20:20 +0300

    Update start.py

--- Bikash/utils/inline/start.py
+++ Bikash/utils/inline/start.py
@@ -15,22 +15,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="⚙ 𝐁𝐨𝐭 𝐒𝐞𝐭𝐭𝐢𝐧𝐠 ⚙", callback_data="settings_helper"
+                text="⚙ اعدادات البوت ⚙", callback_data="settings_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="💖 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 💖", url=config.SUPPORT_CHANNEL
+                text="💖 قناتي 💖", url=config.SUPPORT_CHANNEL
             ),
             InlineKeyboardButton(
-                text="💖 𝐆𝐫𝐨𝐮𝐩 💖", url=config.SUPPORT_GROUP
+                text="💖 الدعم 💖", url=config.SUPPORT_GROUP
             )
-        ],
-        [           
-            InlineKeyboardButton(
-                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://youtube.com/@BikashGadgetsTech"
-            )
-        ],
+        ], 
      ]
     return buttons
 
@@ -39,31 +34,18 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="➕ ❰ 𝐀𝐝𝐝 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 ❱ ➕",
+                text="➕ ❰أضفني الى جروبك❱ ➕",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="💖 𝐇𝐞𝐥𝐩 💖", callback_data="settings_back_helper"
+                text="💖 مساعده 💖", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="🥀 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 💥", url=config.SUPPORT_CHANNEL
-            ),
-            InlineKeyboardButton(
-                text="🥀 𝐆𝐫𝐨𝐮𝐩 💥", url=config.SUPPORT_GROUP
-            )
-        ],
-        [           
-            InlineKeyboardButton(
-                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://youtube.com/@BikashGadgetsTech"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="♕ 𝐎𝐰𝐧𝐞𝐫 ♕", user_id=OWNER
+                text="♕ المطور ♕", user_id=OWNER
             )
         ]
      ]
